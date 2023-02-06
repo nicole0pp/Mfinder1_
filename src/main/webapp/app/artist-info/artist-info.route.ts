91772b3ae8cb726613eb38bdd73c12ecbbb630dd
nicole0pp <nicolestudio16@gmail.com>
2023-02-06 14:21:03 +0100

    Nahda

--- src/main/webapp/app/artist-info/artist-info.route.ts
+++ src/main/webapp/app/artist-info/artist-info.route.ts
@@ -5,7 +5,4 @@ import { ArtistViewComponent } from './artist-info.component';
 export const ARTISTVIEW_MODULE: Route = {
   path: '',
   component: ArtistViewComponent,
-  data: {
-    pageTitle: 'artist-info.title',
-  },
 };
