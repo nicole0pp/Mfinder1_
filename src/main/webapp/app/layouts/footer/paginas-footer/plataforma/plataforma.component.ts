87c5e2d9f655981705d9c962649d3d8ff6a1fdbb
DESKTOP-401AUL8\melit <nicole.pena@melit.es>
2023-04-05 09:21:37 +0200

    creacion de la vista para "Plataforma"

---
+++ src/main/webapp/app/layouts/footer/paginas-footer/plataforma/plataforma.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Account } from 'app/core/auth/account.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { Subject, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'plataforma.component',
+  templateUrl: './plataforma.component.html',
+  styleUrls: ['../../paginas-footer/paginas-footer.scss'],
+})
+export class PlataformaComponent implements OnInit, OnDestroy {
+  account: Account | null = null;
+
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(private accountService: AccountService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => (this.account = account));
+  }
+
+  login(): void {
+    this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
