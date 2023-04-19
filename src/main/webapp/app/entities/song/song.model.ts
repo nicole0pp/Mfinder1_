b1bc89e6ef7752d2c8ad19f0961d179386aaf432
nicole0pp <nicolestudio16@gmail.com>
2023-04-19 12:32:01 +0200

    poner el input para que coja las duraciones de las canciones

--- src/main/webapp/app/entities/song/song.model.ts
+++ src/main/webapp/app/entities/song/song.model.ts
@@ -1,13 +1,14 @@
 import { IListDetails } from 'app/entities/list-details/list-details.model';
 import { IAlbum } from 'app/entities/album/album.model';
 import { MusicGenre } from 'app/entities/enumerations/music-genre.model';
+import { Duration } from 'dayjs/esm/plugin/duration';
 
 export interface ISong {
   id: number;
   name?: string | null;
   picture?: string | null;
   pictureContentType?: string | null;
-  duration?: string | null;
+  duration?: Duration | null;
   audio?: string | null;
   audioContentType?: string | null;
   artists?: string | null;
