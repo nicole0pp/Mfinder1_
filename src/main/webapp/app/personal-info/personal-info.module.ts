59977e9a6f9f57099ec4bc2f8d1b2598f678cece
nicole0pp <nicolestudio16@gmail.com>
2023-05-17 13:31:35 +0200

    Traducciones de vista choose y linkeo para el registro de personal

--- src/main/webapp/app/personal-info/personal-info.module.ts
+++ src/main/webapp/app/personal-info/personal-info.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { PERSONAL_MODULE } from './personal-info.route';
 import { PersonalComponent } from './personal-info.component';
+import { AppRoutingModule } from 'app/app-routing.module';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([PERSONAL_MODULE])],
+  imports: [SharedModule, RouterModule.forChild([PERSONAL_MODULE]), AppRoutingModule],
   declarations: [PersonalComponent],
+  exports: [RouterModule],
 })
 export class PersonsalModule {}
